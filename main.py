4d146a960509e19849df12d84d65f46dc697768a
Bella Bean <gbean4@byu.edu>
2025-04-10 12:02:06 -0600

    fix uploading df

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 @st.cache_data
 def load_name_data():
     # Load the dataset from a CSV file
-    df = pd.read_csv('https://raw.githubusercontent.com/gbean4/Post_2/9b874da1c45720f3196d7f1bd7edc4a60ee30484/fast_food_analysis.csv')    return df
+    df = pd.read_csv('https://raw.githubusercontent.com/gbean4/Post_2/9b874da1c45720f3196d7f1bd7edc4a60ee30484/fast_food_analysis.csv')    
+    return df
 
 df = load_name_data()
 
